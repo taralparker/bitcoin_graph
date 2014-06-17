5b20debcb7d9538e1360bd2a81a09de78c666ebf
Tara Parker <qte@macwf02902.ornl.gov>
2014-06-17 13:49:47 -0400

    Changed formatting

--- preprocess_node_data.py
+++ preprocess_node_data.py
@@ -1,49 +1,60 @@
+## Create TAB seperated csv file(s)
+## For multiple files for nodes, comma separated, without spaces like "node1.csv,node2.csv"
+## (Optional) Valid types: int, long, float, double, boolean, byte, short, char, string
+## (Optional) Specify concrete node-id's with: i:id
+##
+## Example File:
+##
+## name:string:users       age     works_on
+## Michael 37      neo4j
+## Selina  14
+
+## Parse and format the USER nodes into a new file
 def parse_userkey():
 	inputfile = open("userkey_list.txt", "r")
-	outputfile = open(output,"a")
-
-	i=0	
- 	for line in iter(inputfile):
-		i += 1
-		outputfile.write("N "+"UserNode "+"User-"+str(i)+";\n")
-		#for testing
-#		print ("N "+"UserNode "+"User-"+str(i)+";\n")
-
-
-	inputfile.close()
-	outputfile.close()
-
-def parse_pubkey():
-	inputfile = open("pubkey_list.txt", "r")
-	outputfile = open(output,"a")
+	outputfile = open("nodes1.csv","a")
 
 	i=0
+	## Property names in first row
+	outputfile.write("user:string:users\ttype\n")	
  	for line in iter(inputfile):
 		i += 1
+		outputfile.write("User-"+str(i)+"\tUserNode\n")
+	inputfile.close()
+	outputfile.close()
+
+
+## Parse and format the PUBKEY nodes into a new file
+def parse_pubkey():
+	inputfile = open("pubkey_list.txt", "r")
+	outputfile = open("nodes2.csv","a")
+
+	i=0
+	## Property names in first row
+ 	outputfile.write("pubkey:string:pubkeys\ttype\tkey\n")
+	for line in iter(inputfile):
+		i += 1
 		line = line.strip()	
-		outputfile.write("N "+"PubkeyNode "+"Pubkey-"+str(i)+";"+" pubkey:\""+line+"\";\n")
-		#for testing
-#		print ("N "+"PubkeyNode "+"Pubkey-"+str(i)+";"+" pubkey:"+line+";\n")
-
+		outputfile.write("Pubkey-"+str(i)+"\tPubkeyNode\t"+line+"\n")
 	inputfile.close()
 	outputfile.close()
 
-##NOT FINISHED..........
+
+## Parse and format the TRANSACTION nodes into a new file
 def parse_transaction():
-	inputfile = open("transaction_data_and_key.txt", "r")
-	outputfile = open(output,"a")
+	## transaction_data_and_key is created by jointxn.py
+	inputfile = open("transactionkey_list.txt", "r")
+	outputfile = open("nodes3.csv","a")
 
+	i=1
+	##Property names in first row
+	outputfile.write("transaction:string:transactions\ttype\ttxn\n")
  	for line in iter(inputfile):
-		index = line.strip().split(",")
-		outputfile.write("N "+"TxnNode "+"Txn-"+str(index[0])+";"+" txn:\""+str(index[5])+"\", date:"+str(index[3])+", value:"+str(index[4])+";\n")	
-		#for testing
-#		print ("N "+"TxnNode "+"Txn-"+str(index[0])+";"+" txn:\""+str(index[5])+"\", date:"+str(index[3])+", value:"+str(index[4])+";\n")	
-		
+		outputfile.write("Txn-"+str(i)+"\tTxnNode"+"\t"+line)	
+		i += 1
 	inputfile.close()
 	outputfile.close()
 
-#Name of the output node file
-output = "nodes.txt"
 
 parse_userkey()
 parse_pubkey()
--- preprocess_relationship_data.py
+++ preprocess_relationship_data.py
@@ -1,52 +1,78 @@
-def parse_user_edges():
-	inputfile = open("transaction_data_and_key.txt","r")
-	outputfile = open(output, "a")
+## Create TAB seperated csv file(s)
+## For multiple files for rels, comma separated, without spaces like "rels1.csv,rels2.csv"
+## (Optional) Valid types: int, long, float, double, boolean, byte, short, char, string
+## (Optional) Specify concrete node-id's with: i:id
+## 
+## Example file
+##
+## name:string:users       name:string:users       type    since   counter:int
+## Michael Selina  FATHER_OF       1998-07-10      1
+## Michael Rana    FATHER_OF       2007-09-15      2
+## Michael Selma   FATHER_OF       2008-05-03      3
+## Rana    Selma   SISTER_OF       2008-05-03      5
+## Selina  Rana    SISTER_OF       2007-09-15      7
 
+## TRANSACTION to USER rels file 
+def parse_user_edges():
+	## transaction_data_and_key.txt is created by jointxn.py
+	inputfile = open("transaction_data_and_key.txt","r")
+	outputfile = open("rels1.csv", "a")
+
+	## Property names in first row
+	outputfile.write("transaction:string:transactions\tuser:string:users\ttype\tdate\tvalue\n")
 	for line in iter(inputfile):
 		index = line.strip().split(",")
-		outputfile.write("R "+str(index[0]+"; OUTPUT_TO;"+str(index[2]))+";\n")
-		outputfile.write("R "+str(index[0]+"; INPUT_FROM;"+str(index[1]))+";\n")
-
-		#for testing
-		print ("R "+str(index[0]+"; OUTPUT_TO;"+str(index[2]))+";\n")
-		print ("R "+str(index[0]+"; INPUT_FROM;"+str(index[1]))+";\n")
-
-
+		outputfile.write("Txn-"+str(index[0])+"\tUser-"+str(index[2])+"\tOUTPUT_TO\t"+str(index[3])+"\t"+str(index[4])+"\n")
+		outputfile.write("Txn-"+str(index[0])+"\tUser-"+str(index[1])+"\tINPUT_FROM\t"+str(index[3])+"\t"+str(index[4])+"\n")
 	inputfile.close()
 	outputfile.close()
 
+## TRANSACTION to TRANSACTION rels file
 def parse_user_edge_inputs():
 	inputfile = open("user_edge_inputs.txt","r")
-	outputfile = open(output, "a")
+	outputfile = open("rels2.csv", "a")
 
+	## Property names in first row
+	outputfile.write("transaction:string:transactions\ttransaction:string:transactions\ttype\n")
 	for line in iter(inputfile):
 		index = line.strip().split(",")
 		for i in range(len(index)-1):
-			outputfile.write("R "+str(index[0])+"; INPUT_FROM; "+str(index[i+1])+";\n")
-#			print ("R "+str(index[0])+"; INPUT_FROM; "+str(index[i+1])+";\n")
-	 
+			outputfile.write("Txn-"+str(index[0])+"\tTxn-"+str(index[i+1])+"\tINPUT_FROM\n")	 
 	inputfile.close()
 	outputfile.close()
 
+## TRANSACTION to PUBLIC KEY rels file
 def parse_user_edge_inputs_public_keys():
-	inputfile = open("user_edge_inputs_public_keys.txt","r")
-	outputfile = open(output, "a")
+	inputfile = open("user_edge_input_public_keys.txt","r")
+	outputfile = open("rels3.csv", "a")
 
+	## Property names in first row
+	outputfile.write("transaction:string:transactions\tpubkey:string:pubkeys\ttype\n")
 	for line in iter(inputfile):
 		index = line.strip().split(",")
 		for i in range(len(index)-1):
-			outputfile.write("R "+str(index[0])+"; INPUT_FROM; "+str(index[i+1])+";"\n)
-#			print ("R "+str(index[0])+"; INPUT_FROM; "+str(index[i+1])+";\n")
-
+			outputfile.write("Txn-"+str(index[0])+"\tPubkey-"+str(index[i+1])+"\tINPUT_FROM\n")
 	inputfile.close()
 	outputfile.close()
 
+## PUBLIC KEY to USER rels file
+def parse_userkey_list():
+	inputfile = open("userkey_list.txt", "r")
+	outputfile = open("rels4.csv", "a")
 
+	##Property names in first row
+	outputfile.write("pubkey:string:pubkeys\tuser:string:users\ttype\n")
+	i = 1
+	for line in iter(inputfile):
+		index = line.strip().split(",")
+		for k in range(len(index)-1):
+			outputfile.write("Pubkey-"+str(index[k+1])+"\tUser-"+str(i)+"\tBELONGS_TO\n")
+		i += 1 
+	inputfile.close()
+	outputfile.close()
 
-			
-#Name of the output node file
-output = "relationships.txt"
+parse_user_edges()
+parse_user_edge_inputs()
+parse_user_edge_inputs_public_keys()
+parse_userkey_list()
 
-parse_user_edge()
-#parse_user_edge_inputs()
-#parse_user_edge_inputs_public_keys()
